aa57b0d3c62e456d0f69afdd5bc0be2e2bb2a8bc
bhumit.b <bhumit.bhadani@spaceo.in>
2024-10-18 18:23:49 +0530

    seed file for create data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create(first_name: "Admin", last_name: "User", role: 0, email: "admin@email.com", password: "123456")
+user1.add_role(:admin)
+user2 = User.create(first_name: "User", last_name: "One", email: "user1@email.com", password: "123456")
+user2.add_role(:user)
+
+["Fiction", "Non-Fiction", "Children’s Literature", "Poetry", "Graphic Novels/Comics", "Classics", "Religious/Spiritual", "Anthology/Short Stories"].each do |name|
+  Category.create(name: name)
+end
