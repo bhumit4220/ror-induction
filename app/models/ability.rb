7688ece928302a3980d549e107c34ff262d67f0b
bhumit.b <bhumit.bhadani@spaceo.in>
2024-10-18 18:25:40 +0530

    authorize user for book actions

---
+++ app/models/ability.rb
@@ -0,0 +1,15 @@
+class Ability
+  include CanCan::Ability
+  def initialize(user)
+    return unless user.present?
+    if user.admin?
+      can :manage, Book
+    else
+      can :read, Book
+    end
+  end
+
+  def unauthorized_message(action, subject)
+    I18n.t('unauthorized.default')
+  end
+end
