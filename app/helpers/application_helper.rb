2b18ea2c1c3ece81b8ebd923a88dd3564982797b
bhumit.b <bhumit.bhadani@spaceo.in>
2024-10-18 18:22:21 +0530

    helper method for show flash class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# app/helpers/application_helper.rb
 module ApplicationHelper
+  def flash_class(type)
+    case type.to_sym
+    when :notice then 'alert-info'
+    when :success then 'alert-success'
+    when :error then 'alert-danger'
+    when :alert then 'alert-warning'
+    else 'alert-info'
+    end
+  end
 end
