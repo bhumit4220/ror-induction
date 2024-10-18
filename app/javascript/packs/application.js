40572bf59285bd59a1559d087ddd0e0c453897a2
bhumit.b <bhumit.bhadani@spaceo.in>
2024-10-18 18:24:44 +0530

    added jquery and bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import 'bootstrap';
+import '@popperjs/core';
+import "@fortawesome/fontawesome-free/js/all";
 
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+import $ from 'jquery';
+// Expose jQuery globally
+window.$ = $;
+window.jQuery = $;
