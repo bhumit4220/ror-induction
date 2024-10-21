41d9463641f5e0f1f058ff5af1a973eab6fb4425
bhumit.b <bhumit.bhadani@spaceo.in>
2024-10-21 18:40:01 +0530

    add necessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,30 @@
 Rails.application.routes.draw do
   root "books#index"
   devise_for :users
-  resources :books
+  resources :authors
+  resources :users, only: [:index] do
+    get :favorite_books, on: :member
+  end
+  resources :books do
+    put :favorite, on: :member
+    collection do
+      get :report
+    end
+  end
+  get '/api' => redirect('/swagger/dist/index.html?url=/apidocs/api-docs.json')
+
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      devise_for :users, skip: [:sessions, :passwords]
+      as :user do
+        post 'register', to: 'users#registration'
+      end
+      resources :users do
+        collection do
+          post :log_in
+          post :log_out
+        end
+      end
+    end
+  end
 end
