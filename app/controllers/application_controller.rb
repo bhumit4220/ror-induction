dec75d877759d1a11ef409626266d0923cdc4c9e
bhumit.b <bhumit.bhadani@spaceo.in>
2024-10-21 18:19:22 +0530

    override after_sign_in_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,14 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    if resource.user?
+      books_path 
+    else
+      report_books_path
+    end
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
