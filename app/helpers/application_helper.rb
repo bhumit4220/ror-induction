870ec7fa6345d1599dc49e0800790f578e6e4a16
bhumit.b <bhumit.bhadani@spaceo.in>
2024-10-21 18:37:34 +0530

    helper methods for mark current page link active in header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,25 @@ module ApplicationHelper
     else 'alert-info'
     end
   end
+
+  def user_added_book_as_favourite(user, book)
+    user.favorite_books.include?(book)
+  end
+  
+  def navbar_color(user)
+    return "dark" unless user
+    return "secondary" if user.admin?
+    "dark"
+  end
+
+  def active_class(contoller, klass, action)
+    return if EXCLUDED_ACTIONS.include?(action)
+    contoller == klass ? "active" : ""
+  end
+
+  def active_action_class(action, klass)
+    action == klass ? "active" : ""
+  end
+
+  EXCLUDED_ACTIONS = ["report", "favorite_books"]
 end
