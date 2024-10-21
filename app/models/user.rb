030a864cb61ff56cc03b1b038bc3825135379f96
bhumit.b <bhumit.bhadani@spaceo.in>
2024-10-21 18:39:43 +0530

    add methods for encrypt/decrypt devise token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require 'jwt'
 class User < ApplicationRecord
   rolify
   # Include default devise modules. Others available are:
@@ -5,14 +6,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :favorites, dependent: :destroy
   has_many :favorite_books, through: :favorites, source: :book
-
+  has_many :devise_tokens
   enum role: %i[Admin User]
   after_create :assign_default_role
-
+  
   def full_name
     [first_name, last_name].join(" ")
   end
-
+  
   def admin?
     has_role?(:admin)
   end
@@ -20,6 +21,14 @@ class User < ApplicationRecord
   def user?
     has_role?(:user)
   end 
+  
+  def genrate_device_token
+    devise_tokens.create(auth_token: Jwt.new(id, nil).generate_token)
+  end
+
+  def user_roles
+    roles.pluck(:name).join(", ")
+  end
 
   private
 
