ad0fc47752476792021d0905a51399565cb2586c
bhumit.b <bhumit.bhadani@spaceo.in>
2024-10-22 16:38:29 +0530

    add message and user model association

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,12 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_books, through: :favorites, source: :book
   has_many :devise_tokens
+  has_many :sent_messages, class_name: 'Message', foreign_key: 'sender_id'
+  has_many :received_messages, class_name: 'Message', foreign_key: 'receiver_id'
   enum role: %i[Admin User]
   after_create :assign_default_role
-  
+  after_update :broadcast_online_status, if: :saved_change_to_online?
+
   def full_name
     [first_name, last_name].join(" ")
   end
@@ -33,6 +36,10 @@ class User < ApplicationRecord
   private
 
   def assign_default_role
-    self.add_role(:user) if roles.blank? # Assign 'user' role by default
+    self.add_role(:user) if roles.blank?
+  end
+
+  def broadcast_online_status
+    ActionCable.server.broadcast "users_online_channel", { user_id: id, online: online }
   end
 end
