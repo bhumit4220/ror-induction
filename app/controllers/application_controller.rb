8251bd54a570425c4039c449e9ea4bce2d947811
bhumit.b <bhumit.bhadani@spaceo.in>
2024-10-23 15:34:54 +0530

    update user admin status after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
+    resource.update(online: true)
     if resource.user?
-      resource.update(online: true)
       books_path 
     else
       report_books_path
